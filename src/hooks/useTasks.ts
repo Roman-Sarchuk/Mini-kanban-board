81d7db0a2d0b4925cb2a529ce2c7448c49d9f294
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2026-02-20 20:52:50 +0200

    feat: add tasks crud hook

---
+++ src/hooks/useTasks.ts
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react'
+import type { Task } from '../types'
+
+const STORAGE_KEY = 'my-kanban-tasks'
+
+export function useTasks() {
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const stored = localStorage.getItem(STORAGE_KEY)
+    if (!stored) return []
+
+    try {
+      return JSON.parse(stored) as Task[]
+    } catch {
+      return []
+    }
+  })
+
+  // For dev only: clear tasks in local storage on every reload
+  // useEffect(() => {
+  //   setTasks([]);
+  // }, [])
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks))
+  }, [tasks])
+
+  const addTask = (columnId: string, title: string) => {
+    const tasksInColumn = tasks.filter(t => t.columnId === columnId)
+
+    const newTask: Task = {
+      id: crypto.randomUUID(),
+      columnId: columnId,
+      order: tasksInColumn.length,
+      title: title,
+    }
+
+    setTasks(prev => [...prev, newTask])
+  }
+
+  const updateTask = (id: string, updates: Partial<Omit<Task, 'id'>>) => {
+    setTasks(prev =>
+      prev.map(task =>
+        task.id === id ? { ...task, ...updates } : task
+      )
+    )
+  }
+
+  const deleteTask = (id: string) => {
+    setTasks(prev => {
+      const taskToDelete = prev.find(t => t.id === id)
+      if (!taskToDelete) return prev
+
+      const filtered = prev.filter(t => t.id !== id)
+
+      return normalizeOrder(filtered, taskToDelete.columnId)
+    })
+  }
+
+  // const moveTask = (
+  // id: string,
+  // newColumnId: string,
+  // newOrder: number
+  // ) => {
+  //   setTasks(prev => {
+  //     const task = prev.find(t => t.id === id)
+  //     if (!task) return prev
+  //     if (task.columnId === newColumnId && task.order === newOrder) return prev
+
+  //     let updatedTasks = prev.map(t =>
+  //       t.id === id ? { ...t, state: newState, order: newOrder } : t
+  //     )
+
+  //     // normalized old column
+  //     updatedTasks = normalizeOrder(updatedTasks, task.columnId)
+
+  //     if (task.state !== newState) {
+  //       // normalized new column
+  //       updatedTasks = normalizeOrder(updatedTasks, newState)
+  //     }
+
+  //     return updatedTasks
+  //   })
+  // }
+
+  return { tasks, addTask, updateTask, deleteTask }
+}
+
+function normalizeOrder(tasks: Task[], columnId: string): Task[] {
+  const columnTasks = tasks
+    .filter(t => t.columnId === columnId)
+    .sort((a, b) => a.order - b.order)
+
+  return tasks.map(task => {
+    if (task.columnId !== columnId) return task
+
+    const index = columnTasks.findIndex(t => t.id === task.id)
+
+    return { ...task, order: index }
+  })
+}
